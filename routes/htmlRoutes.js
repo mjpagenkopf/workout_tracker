4edac931afb8738bf7f975b38ed775db6b9ae3d5
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-06-21 18:13:42 -0400

    connected routes to html pages in public folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 //2 of 3 to complete: finish these html routes
-router.get('/')
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/index.html"));
+  });
 
-router.get('/stats')
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/stats.html"));
+  });
 
-router.get('/workout')
+router.get('/workout', (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/workout.html"));
+  });
 
 module.exports = router;
