9ca161e9ccf23e047ab125ce9a2b70a9e43ddf25
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-07-28 02:10:51 -0400

    deploying

--- public/api.js
+++ public/api.js
@@ -1,24 +1,23 @@
 const API = {
   async getLastWorkout() {
-    let res;
     try { //get workouts
-      res = await fetch("/api/workouts");
+      const res = await fetch("/api/workouts");
+      const json = await res.json();
+      return json
     } catch (err) {
       console.log(err)
     }
-    const json = await res.json();
-
-    return json[json.length - 1];
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
 
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT", //update workouts
-      headers: { "Content-Type": "application/json" },
+      headers: { 
+        "Content-Type": "application/json"
+       },
       body: JSON.stringify(data)
     });
-
     const json = await res.json();
 
     return json;
@@ -27,18 +26,18 @@ const API = {
     const res = await fetch("/api/workouts", {
       method: "POST", //create new
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers: { 
+        "Content-Type": "application/json"
+       }
     });
-
     const json = await res.json();
 
     return json;
   },
 
   async getWorkoutsInRange() { //last 7 workouts
-    const res = await fetch(`/api/workouts/range`);
+    const res = await fetch('/api/workouts/range');
     const json = await res.json();
-
     return json;
   },
 };
